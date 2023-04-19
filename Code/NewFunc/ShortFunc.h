0e96ddb0db5221ae2fca36f6d83cd660cb380101
cs <cs1597616514@gmail.com>
2023-04-19 11:05:49 +0800

    first

---
+++ Code/NewFunc/ShortFunc.h
@@ -0,0 +1,13 @@
+#ifndef SHORTFUNC_H
+#define SHORTFUNC_H
+
+
+
+
+extern UINT8 test1;
+extern UINT8 test2;
+extern UINT8 test3;
+extern UINT8 test4;
+void InitShortCur(void);
+
+#endif	/* SHORTFUNC_H */
